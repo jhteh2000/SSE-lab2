36b9b7ec58cccd1aab4c201f1bad2963e0293144
Jing Hong Teh <jing.teh23@imperial.ac.uk>
2023-10-16 16:47:38 +0100

    Added rock paper scissors game

--- api/app.py
+++ api/app.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
 
-@app.route("/submit", methods=["POST"])
-def submit():
-    input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)
+@app.route("/rockpaperscissors", methods=["GET", "POST"])
+def rockpaperscissors():
+    input = request.form.get("input")
+    bot_input = random.choice(["rock", "paper", "scissors"])
+
+    if (
+        (input == "rock" and bot_input == "scissors")
+        or (input == "paper" and bot_input == "rock")
+        or (input == "scissors" and bot_input == "paper")
+    ):
+        output = "You Win!"
+    elif (
+        (input == "rock" and bot_input == "rock")
+        or (input == "paper" and bot_input == "paper")
+        or (input == "scissors" and bot_input == "scissors")
+    ):
+        output = "It's a draw!"
+    elif input is None:
+        output = input = bot_input = ""
+    else:
+        output = "You Lose!"
+
+    return render_template(
+        "rockpaperscissors.html", input=input, bot_input=bot_input, result=output
+    )
