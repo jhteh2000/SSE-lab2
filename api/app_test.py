8392063556e3c8e1584828d8b14e57d6b2e2245f
gene0524 <79795127+gene0524@users.noreply.github.com>
2023-10-30 16:50:44 +0000

    Update app_test.py

--- api/app_test.py
+++ api/app_test.py
@@ -15,7 +15,7 @@ def test_knows_about_plus():
 
 
 def test_knows_about_multiplication():
-    assert process_query("What is 27 multiplied by 34?") == 918
+    assert process_query("What is 27 multiplied by 34?") == "918"
 
 
 def test_does_not_know_about_asteroids():
