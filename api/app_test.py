e28a6de7b4903a0372591e3dd062ff760ae61a63
gy123 <gy123@imperial.ac.uk>
2023-10-30 16:15:12 +0000

    update

--- api/app_test.py
+++ api/app_test.py
@@ -9,18 +9,13 @@ million years ago"
 def test_knows_about_team_name():
     assert process_query("What is your name?") == "Team_team"
 
+
 def test_knows_about_plus():
     assert process_query("What is 6 plus 9?") == "15"
 
+
 def test_knows_about_multiplication():
     assert process_query("What is 27 multiplied by 34?") == "918"
-
-def test_knows_plus():
-    assert process_query("What is 6 plus 9?") == '15'
-
-
-def test_knows_multiplied():
-    assert process_query("What is 10 multiplied by 20?") == '200'
 
 
 def test_does_not_know_about_asteroids():
